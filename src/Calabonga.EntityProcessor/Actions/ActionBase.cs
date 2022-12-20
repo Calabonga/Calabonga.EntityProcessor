6439e0dd033711bc33940af1e288dab467c00672
Sergei Pukhovnikov <pukhovnikov@skbkontur.ru>
2022-12-20 11:18:22 +0700

    DocFX updated

--- src/Calabonga.EntityProcessor/Actions/ActionBase.cs
+++ src/Calabonga.EntityProcessor/Actions/ActionBase.cs
@@ -1,7 +1,18 @@
 ﻿namespace Calabonga.EntityProcessor.Actions;
 
+/// <summary>
+/// Базовый класс реализующий действия над сущностью. Действия могут быть получены из контейнера, так и созданы явно в месте запуска.
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
 public abstract class ActionBase<TEntity> : IAction<TEntity> where TEntity : class
 {
+    /// <summary>
+    /// Возвращает результат применения действия <see cref="EntityActionResult"/> для сущности. Реализация этого метода
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <param name="context"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public abstract Task<EntityActionResult> ApplyAsync(TEntity entity, EntityProcessorContext context, CancellationToken cancellationToken);
 
     public virtual bool IsShouldBeHandled { get; } = false;
--- src/Calabonga.EntityProcessor/DomainEventCollectionBase.cs
+++ src/Calabonga.EntityProcessor/DomainEventCollectionBase.cs
@@ -5,17 +5,26 @@ namespace Calabonga.EntityProcessor;
 
 /// <summary>
 /// Коллекция зарегистрированных событий домена для контекста <see cref="EntityProcessorContext"/> определяет возможность
-/// регистрировать события домена <see cref="IDomainEvent"/> в процессе обработки команд <see cref="IAction{TEntity}"/>
+/// регистрировать события домена <see cref="IDomainEvent"/> в процессе обработки команд <see cref="IAction{TEntity}"/>.
+/// Реализация интерфейса <seealso cref="IHaveDomainEvents"/>
 /// </summary>
+/// <remarks>
+/// При разработки плагинов для включения их в конвейер обработки команд <see cref="IAction{TEntity}"/> можно
+/// наследовать класс <see cref="DomainEventCollectionBase"/>, который предоставить возможность
+/// регистрировать события домена для последующей обработки.
+/// </remarks>
 public abstract class DomainEventCollectionBase : IHaveDomainEvents
 {
     private readonly List<IDomainEvent> _domainEvents = new();
 
-    /// <inheritdoc />
+    /// <summary>
+    /// Зарегистрированные события домена <see cref="IDomainEvent"/>
+    /// </summary>
     public IEnumerable<IDomainEvent> DomainEvents => _domainEvents;
 
     /// <summary>
-    /// Регистрирует новое событие домена в списке событий на исполнения после выполнения <see cref="IAction{TEntity}"/> при условии, что включена обработка в конфигурации <see cref="EntityProcessorConfiguration.AutoFireDomainEvents"/>
+    /// Регистрирует новое событие домена в списке событий на исполнения после выполнения <see cref="IAction{TEntity}"/> при условии,
+    /// что включена обработка в конфигурации <see cref="EntityProcessorConfiguration.AutoFireDomainEvents"/>
     /// </summary>
     /// <param name="domainEvent">событие домена</param>
     public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
