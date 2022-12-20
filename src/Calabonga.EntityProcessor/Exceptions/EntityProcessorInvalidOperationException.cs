ed4fcb989b4ec818a81b6a275469f2565a637062
Sergei Pukhovnikov <pukhovnikov@skbkontur.ru>
2022-12-20 14:01:04 +0700

    DocFX updated

--- src/Calabonga.EntityProcessor/Exceptions/EntityProcessorInvalidOperationException.cs
+++ src/Calabonga.EntityProcessor/Exceptions/EntityProcessorInvalidOperationException.cs
@@ -1,7 +1,18 @@
 ﻿namespace Calabonga.EntityProcessor.Exceptions;
 
+/// <summary>
+/// Исключение, которое выкидывает процессор <see cref="EntityProcessorBase{TEntity}"/>, когда операция не может
+/// быть выполнено. 
+/// </summary>
+/// <remarks>
+/// Исключение могут быть при проверке правил или при применение действия, а также если не было найдено зарегистрированных в DI-контейнере, например. 
+/// </remarks>
 public class EntityProcessorInvalidOperationException : Exception
 {
+    /// <summary>
+    /// Создает экземпляр класса исключения.
+    /// </summary>
+    /// <param name="message">сообщение об ошибке</param>
     public EntityProcessorInvalidOperationException(string? message) : base(message)
     {
 
--- src/Calabonga.EntityProcessor/Extensions/ServiceCollectionExtensions.cs
+++ src/Calabonga.EntityProcessor/Extensions/ServiceCollectionExtensions.cs
@@ -2,8 +2,17 @@
 
 namespace Calabonga.EntityProcessor.Extensions;
 
+/// <summary>
+/// Расширение для контейнера <see cref="IServiceCollection"/>, которое регистрирует процессов и его конфигурацию в контейнере.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Расширение для контейнера <see cref="IServiceCollection"/>, которое регистрирует процессов и его конфигурацию в контейнере.
+    /// </summary>
+    /// <param name="services">сервисы</param>
+    /// <param name="configure">конфигурация</param>
+    /// <typeparam name="TProcessor">процессор</typeparam>
     public static void AddEntityProcessor<TProcessor>(this IServiceCollection services, Action<EntityProcessorConfiguration> configure)
     {
         services.AddScoped(typeof(TProcessor));
@@ -12,6 +21,12 @@ public static class ServiceCollectionExtensions
         services.AddSingleton(configuration);
     }
 
+    /// <summary>
+    /// Расширение для контейнера <see cref="IServiceCollection"/>, которое регистрирует процессов и его конфигурацию в контейнере.
+    /// </summary>
+    /// <param name="services">сервисы</param>
+    /// <param name="configuration">конфигурация</param>
+    /// <typeparam name="TProcessor">процессор</typeparam>
     public static void AddEntityProcessor<TProcessor>(this IServiceCollection services, EntityProcessorConfiguration? configuration)
     {
         services.AddScoped(typeof(TProcessor));
