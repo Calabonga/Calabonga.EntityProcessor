1b5b95f2320a00b60b3ed6d4f4a1f3cfe2487877
Calabonga <dev@calabonga.net>
2022-12-24 13:33:58 +0700

    some summaries were added

--- src/Calabonga.EntityProcessor/EntityProcessorConfiguration.cs
+++ src/Calabonga.EntityProcessor/EntityProcessorConfiguration.cs
@@ -1,10 +1,25 @@
-﻿namespace Calabonga.EntityProcessor;
+﻿using Calabonga.EntityProcessor.Actions;
+using Calabonga.EntityProcessor.Events;
+using MediatR;
 
-public class EntityProcessorConfiguration<TEntity>: IEntityProcessorConfiguration
+namespace Calabonga.EntityProcessor;
+
+/// <summary>
+/// Общая конфигурация для всех процессоров, зарегистрированных в системе
+/// </summary>
+public class EntityProcessorConfiguration<TEntity> : IEntityProcessorConfiguration
 {
+    /// <summary>
+    /// Если включено, то обрабатывается каждое и дублирующих правил. Сообщение об ошибке не выдается.  
+    /// </summary>
     public bool SkipRuleDuplicates { get; set; }
 
+    /// <summary>
+    /// Если включено, то полученные при обработке команды <see cref="IAction{TEntity}"/> события
+    /// домена <see cref="IDomainEvent"/> будут автоматически запущены через посредника <see cref="IMediator"/> 
+    /// </summary>
     public bool AutoFireDomainEvents { get; set; }
+
 
     public string EntityName => typeof(TEntity).Name;
 }
--- src/Calabonga.EntityProcessor/IEntityProcessorConfiguration.cs
+++ src/Calabonga.EntityProcessor/IEntityProcessorConfiguration.cs
@@ -1,10 +1,24 @@
+﻿using Calabonga.EntityProcessor.Actions;
+using Calabonga.EntityProcessor.Events;
+using MediatR;
+
 namespace Calabonga.EntityProcessor;
 
 public interface IEntityProcessorConfiguration
 {
+    /// <summary>
+    /// Название сущности, для которой создан <see cref="EntityProcessor{TEntity}"/>
+    /// </summary>
     string EntityName { get; }
-    
+
+    /// <summary>
+    /// Если включено, то обрабатывается каждое и дублирующих правил. Сообщение об ошибке не выдается.  
+    /// </summary>
     bool SkipRuleDuplicates { get; }
-    
+
+    /// <summary>
+    /// Если включено, то полученные при обработке команды <see cref="IAction{TEntity}"/> события
+    /// домена <see cref="IDomainEvent"/> будут автоматически запущены через посредника <see cref="IMediator"/> 
+    /// </summary>
     bool AutoFireDomainEvents { get; }
 }
